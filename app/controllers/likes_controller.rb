7c41ee867fcd2cb654e4425b0ae82137be29a72b
ryota.kaneko <kane2508ryo@gmail.com>
2021-05-23 10:35:24 +0000

    timeline.5

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,14 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    review = Review.find(params[:review_id])
+    review = Review.find(params[:like_review_id])
     current_user.like(review)
     flash[:success] = "いいねしました。"
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    review = Review.find(params[:review_id])
+    review = Review.find(params[:like_review_id])
     current_user.unlike(review)
     flash[:success] = "いいねを削除"
     redirect_back(fallback_location: root_path)
